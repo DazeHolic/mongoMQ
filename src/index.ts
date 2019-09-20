d63371d78daeeec3eb2a7eba3eac8a87acf9c7d7
wlj <wlj@rd.nd>
2019-09-20 14:15:57 +0800

    modify

--- src/index.ts
+++ src/index.ts
@@ -1,35 +1,5 @@
+import Connection  from './connection';
 
+let MongoMq = Connection;
 
-import { MongoClient } from 'mongodb';
-import MongoMq from './mongoMq';
-import mongoose from 'mongoose';
-
-
-/*
-let url = 'mongodb://localhost:27017/mubsub_example';
-MongoClient.connect(url, {}, function (err, client) {
-  let db = client.db();
-  let mq = new MongoMq(db, {});
-  let channel = mq.channel("test1", { max: 3 });
-  channel.subscribe('baz', function (message: any) {
-    console.log(message); // => 'baz'
-  });
-});
-*/
-
-let url = 'mongodb://localhost:27017/mubsub_example';
-let opts = {
-  useNewUrlParser: true,
-  useUnifiedTopology: true
-}
-let connect = mongoose.createConnection(url, opts);
-connect.on('connected', function (err) {
-  let mq = new MongoMq(connect, {});
-  let channel = mq.channel("test1", { max: 3 });
-  channel.subscribe('baz', function (message: any) {
-    console.log(message); // => 'baz'
-  });
-});
-
-
-
+export default MongoMq;
--- src/mongoMq/index.ts
+++
@@ -1,5 +0,0 @@
-import Connection  from './connection';
-
-let MongoMq = Connection;
-
-export default MongoMq;
